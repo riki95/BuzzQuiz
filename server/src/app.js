b473811e86e77a05f87b193e63a0ca47641b9716
Fabrizio Silvestri <fabrizio.silvestri@yahoo.it>
2020-05-13 19:11:40 +0200

    [FTR] Update server to serve client application built in static folder

--- server/src/app.js
+++ server/src/app.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const http = require('http');
+const path = require('path');
 
 
 const SERVER_PORT = process.env.PORT || 3000;
 
 const app = express();
 app.set('port', SERVER_PORT);
+app.use('/static', express.static(__dirname + '/static'));
 
+app.get('/', (_req, res) => {
+	res.sendFile(path.join(__dirname + '/static/index.html'));
+});
 
-app.get('/*', (_req, res) => {
+app.get('/api/*', (_req, res) => {
 	res.send('hi');
 });
 
