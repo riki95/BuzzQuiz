02e0a596f1074c9b666b25465a39ba26398811d0
Fabrizio Silvestri <fabrizio.silvestri@yahoo.it>
2020-05-14 20:22:34 +0200

    Install and build client automatically

--- deploy.sh
+++ deploy.sh
@@ -27,10 +27,12 @@ cp -R client/static $DEPLOY_FOLDER/src/
 
 cd $DEPLOY_FOLDER
 git init
+git config --global user.email "automatic@deploy.com"
+git config --global user.name "Script"
 git remote add origin $ORIGIN
 git add .
 git commit -m "Deploy ${DATE}"
-git push -f
+git push --set-upstream origin master -f
 
 # Clean build folders
 cd ..
